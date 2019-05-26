cbd8feff4e11afdcea96436cad9404020b300d1c
NKU16y <m13102251128@163.com>
2019-05-26 12:33:09 +0800

    the final version

--- src/map_update.py
+++ src/map_update.py
@@ -1,26 +1,39 @@
 #! /usr/bin/env python
 # -*- encoding: UTF-8 -*-
+#Author:Yu Shizhuo
 import rospy
 import os
 import sys
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid
+from std_msgs.msg import String
 import math
 import numpy as np
+import cv2
+import tf
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PointStamped
+
 class map_update:
-    
+
     def __init__(self):
         rospy.init_node("map_update")
-        self.map_update_pub=rospy.Publisher("/path_update",OccupancyGrid,queue_size=15)
+        self.map_update_pub=rospy.Publisher("/map",OccupancyGrid,queue_size=15)
+        self.order_pub=rospy.Publisher("/order_to_stop",String,queue_size=10)
         #地图相关
-        self.origin_x = 0
-        self.origin_y = 0
-        self.height = 0
-        self.resolution = 0
-        self.width = 0
+        self.stop_order="Stop now."
+        self.map=[]
         self.map_raw=[]
-        #self.map_sub=rospy.Subscriber("/map",OccupancyGrid,self.mapCallback)
+        #地图原点
+        self.origin_x = 0.0
+        self.origin_y = 0.0
+        self.height = 0
+        self.width = 0
+        self.resolution = 0.0
+        #判断是否接收到地图
+        self.is_map_received=False
+        self.map_sub=rospy.Subscriber("/map",OccupancyGrid,self.mapCallback)
         #激光相关
         self.angle_min=0.0
         self.angle_max=0.0
@@ -28,50 +41,62 @@ class map_update:
         self.scan_time=0.0
         self.range_min=-math.pi/6
         self.range_max=math.pi/6
+        #判断是否检测到障碍物
         self.isob=False
+        self.dis=0.5
         self.laser_sub=rospy.Subscriber("/scan",LaserScan,self.laserCallback)
-        #里程计相关
+        #amcl相关       
         self.x = 0.0
         self.y = 0.0
-        self.wx= 0.0
         self.wy= 0.0
+        self.wx= 0.0
         self.xm= 0.0
         self.ym= 0.0
-        self.theta=0.0
-        self.odo_sub=rospy.Subscriber("/sensors_fusion/odom",Odometry,self.odomCallback)
-        self.dis=0.5
-        #self.update_map()
+        self.amcl_sub=rospy.Subscriber("/amcl_pose",PoseWithCovarianceStamped,self.amclCallback)
+        #判断amcl是否完成定位
+        self.isamcl=False
         rospy.Rate(1)
         rospy.spin()
-    
+
     def World2map(self,xw,yw):
-        xm=xw-self.height-self.origin_y/self.resolution
-        ym=yw-self.origin_x/self.resolution
+        ym=float(xw-self.origin_x)/float(self.resolution)
+        xm=float(yw-self.origin_y)/float(self.resolution)
         return xm,ym
     
+    #更新地图函数:
+
     def update_map(self):
-        if self.isob:
+        if self.is_map_received and self.isob and self.isamcl:
+            self.order_pub.publish(self.stop_order)
             print("update map")
             self.wx=self.x+self.dis*math.cos(self.theta)
             self.wy=self.y+self.dis*math.sin(self.theta)
+            print(self.wx)
+            print(self.wy)
             self.xm,self.ym=self.World2map(self.wx,self.wy)
-            U_map=self.map_raw.reshape((self.height, self.width))
+            self.xm=int(self.xm)
+            self.ym=int(self.ym)
+            U_map=self.map_raw.reshape(self.height,self.width)
             for i in range(self.xm-5,self.xm+5):
                 for j in range(self.ym-5,self.ym+5):
                     U_map[i][j]=0
             Up_map=np.array(U_map,dtype=np.int8)
-            Upp_map=OccupancyGrid(Up_map)
-            self.map_update_pub.publish(Upp_map)
+            self.map.data=list(Up_map.flatten())
+            self.map_update_pub.publish(self.map)
 
-            
+    #回调函数:
+   
     def mapCallback(self, msg):
-        self.origin_x = msg.info.origin.position.y
+        self.origin_x = msg.info.origin.position.x
         self.origin_y = msg.info.origin.position.y
         self.resolution = msg.info.resolution
         self.width = msg.info.width
         self.height = msg.info.height
+        self.map=msg
         self.map_raw=np.array(msg.data,dtype=np.int8)
-
+        self.is_map_received=True
+        print("Map received")
+        
     def laserCallback(self,msg):
         self.angle_min=msg.angle_min
         self.angle_max=msg.angle_max
@@ -87,21 +112,18 @@ class map_update:
                 self.isob=True
             else:
                 self.isob =False
-        print (range_index_start)
-        print (range_index_end)
-        print (min_distance)
-        print (self.isob)
-    
-    def odomCallback(self,msg):
+        print("Laser is ready")
+        self.update_map()
+
+    def amclCallback(self,msg):
+        print(msg.header.frame_id)
+        print(msg.pose.pose.position.x,msg.pose.pose.position.y)
         self.x=msg.pose.pose.position.x
         self.y=msg.pose.pose.position.y
         self.theta=math.acos(1-2*msg.pose.pose.orientation.z*msg.pose.pose.orientation.z)
-        print("odom_data----------'\n'")
-        print(msg.pose.pose.orientation.w)
-        print(msg.pose.pose.orientation.x)
-        print(msg.pose.pose.orientation.y)
-        print(msg.pose.pose.orientation.z)
-        print(math.cos(self.theta))
-        print(self.theta)
+        self.isamcl=True
+        print("amcl is ready")
+
 if __name__ == '__main__':
+    
     map_update()
